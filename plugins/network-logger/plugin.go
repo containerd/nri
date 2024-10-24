eaf78a9afe9ebac28a68d1163dd00183525801a3
Antonio Ojea <aojea@google.com>
2024-10-24 13:05:56 +0000

    api: support Pod IPs
    Allow NRI plugins to read the allocated IPs to the pod on the PodSandbox
    hooks. This is specially useful for networking daemons that depend on the Pod
    IPs assigned via CNI by the container runtime (service mesh, network
    policies, ... ) to create seamless integrations without having to modify
    the existing environment.
    
    Existing network integrations use techniques like:
    
    - CNI chaining (invasive) as the implementation needs to modify the existing
    deployment and can cause disruption due to mutations on the
    existing configurations.
    
    - Read Pod Status, that is too late in the process, since the pod
    lifecycle does not update the pod status until the containers are
    running, but there are applications like network policy
    implemetnations or service meshes that needs this information before
    the pod is completely created.
    
    - Inspect the host namespaces to gather the information, that is very
    intrusive and not portable, creating a tighly coupling between.
    
    Add a nri plugin that can be used as reference implementations for this
    integration, the nri plugin just log the interesting networking details
    of the pod (ips and network namespace) on the different hooks of the pod lifecucle.
    
    Change-Id: Idb9605c719903d0df51c9a1947e0f838870b5c0b
    Signed-off-by: Antonio Ojea <aojea@google.com>

---
+++ plugins/network-logger/plugin.go
@@ -0,0 +1,119 @@
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"flag"
+	"os"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/containerd/nri/pkg/api"
+	"github.com/containerd/nri/pkg/stub"
+)
+
+type plugin struct {
+	stub stub.Stub
+	mask stub.EventMask
+}
+
+var (
+	log *logrus.Logger
+)
+
+func getNetworkNamespace(pod *api.PodSandbox) string {
+	// get the pod network namespace
+	for _, namespace := range pod.Linux.GetNamespaces() {
+		if namespace.Type == "network" {
+			return namespace.Path
+		}
+	}
+	return "<host-network>"
+}
+
+func (p *plugin) Synchronize(_ context.Context, pods []*api.PodSandbox, containers []*api.Container) ([]*api.ContainerUpdate, error) {
+	log.Infof("Synchronized state with the runtime (%d pods, %d containers)...",
+		len(pods), len(containers))
+
+	for _, pod := range pods {
+		log.Infof("pod %s/%s: namespace=%s ips=%v", pod.GetNamespace(), pod.GetName(), getNetworkNamespace(pod), pod.GetIps())
+	}
+
+	return nil, nil
+}
+
+func (p *plugin) Shutdown(_ context.Context) {
+	log.Info("Runtime shutting down...")
+}
+
+func (p *plugin) RunPodSandbox(_ context.Context, pod *api.PodSandbox) error {
+	log.Infof("Started pod %s/%s: namespace=%s ips=%v", pod.GetNamespace(), pod.GetName(), getNetworkNamespace(pod), pod.GetIps())
+	return nil
+}
+
+func (p *plugin) StopPodSandbox(_ context.Context, pod *api.PodSandbox) error {
+	log.Infof("Stopped pod %s/%s: ips=%v", pod.GetNamespace(), pod.GetName(), pod.GetIps())
+	return nil
+}
+
+func (p *plugin) RemovePodSandbox(_ context.Context, pod *api.PodSandbox) error {
+	log.Infof("Removed pod %s/%s: ips=%v", pod.GetNamespace(), pod.GetName(), pod.GetIps())
+	return nil
+}
+
+func (p *plugin) onClose() {
+	log.Infof("Connection to the runtime lost, exiting...")
+	os.Exit(0)
+}
+
+func main() {
+	var (
+		pluginName string
+		pluginIdx  string
+		err        error
+	)
+
+	log = logrus.StandardLogger()
+	log.SetFormatter(&logrus.TextFormatter{
+		PadLevelText: true,
+	})
+
+	flag.StringVar(&pluginName, "network-logger", "", "plugin name to register to NRI")
+	flag.StringVar(&pluginIdx, "idx", "", "plugin index to register to NRI")
+	flag.Parse()
+
+	p := &plugin{}
+	opts := []stub.Option{
+		stub.WithOnClose(p.onClose),
+	}
+	if pluginName != "" {
+		opts = append(opts, stub.WithPluginName(pluginName))
+	}
+	if pluginIdx != "" {
+		opts = append(opts, stub.WithPluginIdx(pluginIdx))
+	}
+
+	if p.stub, err = stub.New(p, opts...); err != nil {
+		log.Fatalf("failed to create plugin stub: %v", err)
+	}
+
+	if err = p.stub.Run(context.Background()); err != nil {
+		log.Errorf("plugin exited (%v)", err)
+		os.Exit(1)
+	}
+}
